e6baac9d9283600dbebd939a0a39827e576fedd4
Henry Shepperd <hshepperd@gmail.com>
2017-03-23 22:28:11 +1100

    Add serial task for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 
 #include <nbavr.cpp>
 
+Stream<char>* stdoutp;
+
 void main() {
     typedef PinC0 ledStripPin;
     typedef TimerCounter1 clockTimer;
+    typedef Usart0 serialUsart;
+
+    StreamBuffer<char, 60> stdout;
+    StreamBuffer<char, 0> stdin;
+
+    stdoutp = &stdout;
+
+    stdout << "start" << endl;
 
     ClockT<clockTimer> clock;
 
+    Serial<serialUsart> serial(stdout, stdin);
     Lamp<ledStripPin> lamp(clock);
 
-    Task* tasks[] = {&lamp};
+    Task* tasks[] = {&serial, &lamp};
 
     nbavr(clock, tasks);
 }
